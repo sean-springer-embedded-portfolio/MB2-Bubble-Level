6fa1ee156abea5a2ac3595e50e46ebc94ea0044f
Sean Springer <ss28@pdx.edu>
2026-02-07 14:17:38 -0800

    Added Rustdocs and updated README.md

--- README.md
+++ README.md
@@ -1,21 +1,56 @@
-# mb2-template: MicroBit 2 Rust embedded project template
+# MB2-Bubble-Level
 
-*[A version of this as a [`cargo-generate`](https://cargo-generate.github.io/cargo-generate/) template is
-available on the `cargo-generate` branch in this repo.]*
+Copyright (c) 2026 Sean Springer
 
-To use this [Github template
-repository](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template):
+Transform the MB2 5x5 LED array into a digital **bubble level** using the `LSM303AGR` IMU
 
-1. Follow the instructions linked above to make a Github repo
-   for your project.
-   
-2. If needed, do the following to set up your tools:
+## User Interface (UI)
 
-       rustup target add thumbv7em-none-eabihf
-       rustup component add llvm-tools
-       cargo install cargo-binutils
-       cargo install --locked probe-rs-tools
+The **bubble level** has two resolution modes: Coarse and Fine. The **bubble level** will begin in Coarse mode where each LED represents
+250mG worth of acceleration (see _How it Works_). Pressing the B btn will switch the level into Fine resolution mode where each LED represents
+25mG work of acceleration (10x sensitivity improvement). The A btn can then be used to return to Coarse mode.
 
-3. Edit this `README.md`, the `Cargo.toml` and the stuff in
-   `src/` to get the names right and the template to what
-   you need.
+Additionally, if the board is upside-down (the LEDs are facing the ground) then the **bubble level** will temporarily disable until it has
+been righted.
+
+Lastly, the bubble refreshrate has been fixed at 200ms (5 frames per second) which provides an adequate update rate while minimizing jitter.
+
+## Mechanics
+
+Coarse mode divides each LED into 250mG bins while Fine mode is effectively 10x more sensitive (25mG per LED). The `TWIM` peripheral
+on the MB2 handles the I2C communication protocol used to query the state of the accelerometer within the `LSM3030AGR`.
+
+The GPIO tasks and events (`GPIOTE`) peripheral is used to connect the GPIO pin state of the A and B buttons to the Nested Vector Interrupt Controller `NVIC`.
+Upon either button press, the `NVIC` will interupt the processor and call a custom built interrupt handler function. This handler function protects against
+potential button bounce by using the MB2 `TIMER1` peripheral to apply a cooldown period of 100ms between interrupts.
+
+## How it Works
+
+I2C two-wire interface (`TWIM`) with EasyDMA is used to communicate with the `LSM3030AGR` accelerometer which is capable
+of producing mG ([milli-Gauss](https://en.wikipedia.org/wiki/Gauss_(unit))) where 1000mG = 9.8m/sec^2 measures the acceleration
+felt by the board in due to the acceleration of Earth's magnetic field. Becuase the accelerometer records this with respect to the
+3-dimensional coordinate system, it can be used to mimic a bubble level.
+
+## Build and Run
+
+Assuming you have an attached MB2 with necessary permissions (see [Rust MB2 Discovery Book](https://docs.rust-embedded.org/discovery-mb2/))  
+then this program can be `flashed` onto the MB2 nRF52820 using
+
+```bash
+cargo embed --release
+```
+
+## Sources
+
+1. [Rust MB2 Discovery Book](https://docs.rust-embedded.org/discovery-mb2/)
+2. [Rustdoc](https://doc.rust-lang.org/rustdoc/what-is-rustdoc.html)
+3. Claude Sonnet 4.5 (free version)
+4. nRF52833 Product Specification v1.6
+5. MicroBit v2.21 Schematic
+6. [Microbit Hal Docs](https://docs.rs/microbit/latest/microbit/hal/index.html)
+
+## License
+
+This program is licensed under the "MIT License". Please  
+see the file `LICENSE` in the source distribution of this  
+software for license terms.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+//! Main.rs
+//! Copyright © 2026 Sean Springer
+//! [This program is licensed under the "MIT License"]
+//! Please see the file LICENSE in the source distribution of this software for license terms.
+//!
+//! Uses the Accelerometer within the LSM303AGR eCompass module to transform the 5x5 LED Array
+//! Into a Bubble Level with selectable resolution settings using the A and B buttons.
+//!
+//! The level will begin in Coarse Mode whereby each LED representes a 250 mG step. Selecting the B
+//! button will switch the level in Fine Mode whereby each LED represents a 25 mG step. Selecting A
+//! will return to Coarse Mode. The LED Refresh rate is fixed at 200ms.
+//!
+//! Note, GPIOTE Interrupt handlers are used to manage the button press actions with debounce logic
+
 #![no_main]
 #![no_std]
 
@@ -24,25 +38,40 @@ use microbit::{
 use critical_section_lock_mut::LockMut;
 use lsm303agr::{AccelMode, AccelOutputDataRate, Lsm303agr};
 
+/// constant refresh rate
 const DISPLAY_REFRESH_RATE_MS: u32 = 200;
+/// MB2 LED grid is 5x5
 const LED_SIZE: usize = 5;
+/// conveience type def
 type LEDState = [[u8; LED_SIZE]; LED_SIZE];
 
-// 100ms at 1MHz count rate.
+/// 100ms at 1MHz count rate.
 const DEBOUNCE_TIME: u32 = 100 * 1_000_000 / 1000;
 
+/// Global Mutable objects: Used inside interrupt handler
 static RESOLUTION: AtomicU8 = AtomicU8::new(BubbleResolution::Coarse as u8);
 static GPIOTE_PERIPHERAL: LockMut<gpiote::Gpiote> = LockMut::new();
 static DEBOUNCE_TIMER: LockMut<Timer<TIMER1>> = LockMut::new();
 
+/// BubbleResolution Enum
+///
+/// Used to define the bubble level resolution state
 enum BubbleResolution {
     Coarse = 0,
     Fine = 1,
 }
 
+/// TryFrom<u8> implementation for BubbleResolution enum
+///
+/// defines the try_form trait for converting a u8 into either
+/// BubbleResolution::Coarse or BubbleResolution::Fine else returns
+/// a unit Error. This implementatoin appears to be the accepted way
+/// to convert a integral type into a C-style Enum.
 impl TryFrom<u8> for BubbleResolution {
+    // return Error type is unit bc seems obvious enough
     type Error = ();
 
+    // Returns unit error unless the u8 value is 0 | 1
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             0 => Ok(BubbleResolution::Coarse),
@@ -52,15 +81,28 @@ impl TryFrom<u8> for BubbleResolution {
     }
 }
 
+/// LEDs Struct
+///
+/// Manages the LED state by calculating the proper LED to be lit based upon accelerometer data.
+/// Also retains the current BubbleResolution mode in mode variable.
 struct LEDs {
     state: LEDState,
     mode: BubbleResolution,
 }
 
+/// Impl LEDs
+///
+/// implements functions for generating a new instance, clearing state,
+/// setting the resolution mode, a custom-built round function, and updating the LED state
+/// using the accelerometer x,y,z mG readings
 impl LEDs {
+    /// mG per LED in mode == BubbleResolution::Coarse
     const COARSE_DIVS: f32 = 250.0;
+    /// mG per LED in mode == BubbleResolution::Fine
     const FINE_DIVS: f32 = 25.0;
 
+    /// Generates a new LEDs struct instance with state set to all zeros (all LEDs off)
+    /// and resolution set to BubbleResolution::Coarse
     fn new() -> Self {
         LEDs {
             state: [[0u8; 5]; 5],
@@ -68,16 +110,25 @@ impl LEDs {
         }
     }
 
+    /// reset the current state to be all zeros (all LEDs off)
     fn clear(&mut self) {
         self.state = [[0u8; 5]; 5];
     }
 
+    /// update the current BubbleResolution mode. Changing the resolution mode will change
+    /// the mG per LED divisions.
+    ///
+    /// BubbleResolution::Coarse -> LEDs::COARSE_DIVS
+    /// BubbleResolution::Fine -> LEDs::Fine_DIVS
     fn set_mode(&mut self, mode: BubbleResolution) {
         self.mode = mode;
     }
 
-    fn round(number: f32) -> u32 {
-        let mut integer: u32 = number as u32; //round down
+    /// custom #![no_std] rounding static method. Given the f32 input number, rounding is performed
+    /// by casting the f32 to an i32 and then calcuting the decimal precision that was lost from
+    /// that downcast. Rounds up if the decimal component is >= 0.5
+    fn round(number: f32) -> i32 {
+        let mut integer: i32 = number as i32; //round down
         let remainder: f32 = number - (integer as f32); //get decimal remainder
         if remainder >= 0.5 {
             integer += 1;
@@ -85,16 +136,25 @@ impl LEDs {
         integer
     }
 
+    /// Clamp extreme mG magnitudes to the edges of the board. If not outside the range of the level,
+    /// invokes the LEDs::round static method to round the division to the nearest pixel
     fn clamp(value: f32) -> usize {
         if value <= 0.0 {
-            0
+            0 //clamp off to the left or off the top
         } else if value >= (LED_SIZE - 1usize) as f32 {
-            LED_SIZE - 1
+            LED_SIZE - 1 //clamp off to the right or bottom
         } else {
+            // cast to usize is safe bc negative values handled above
             LEDs::round(value) as usize
         }
     }
 
+    /// update and return the LED lit state. If the board is upside-down (z > 0),
+    /// returns a cleared state (all LEDs are off). Otherwise, will return a state with
+    /// one and only one LED in the On (lit) state.
+    ///
+    /// Depending upon the resolution mode, transforms the mG accelerometer inputs into a
+    /// pixel value which will be set to the On (lit) state.
     fn update(&mut self, x: i32, y: i32, z: i32) -> LEDState {
         if z > 0 {
             self.clear();
@@ -118,8 +178,16 @@ impl LEDs {
     }
 }
 
+/// GPIOTE Interrupt handler (nrf52833 Peripheral Vector Table Entry #6)
+///
+/// Handles interrupts originating from either the A or B btn press with anti-bouncing logic.
+/// MB2 TIMER1 is used to implement a 100ms cooldown on interrupt handling in order to protect against
+/// button bounce. Then, the sending button is determined by checking which GPIOTE channel triggered the event
+/// (Channel 0 is attached to the A btn and Channel 1 is attached to the B button). The RESOLUTION atomic is
+/// updated using the intergral representation of the appropriate BubblerResolution variant.
 #[interrupt]
 fn GPIOTE() {
+    // check for bouncing using a 100ms timer based coolddown:
     let mut debounced = false;
     DEBOUNCE_TIMER.with_lock(|debounce_timer| {
         if debounce_timer.read() == 0 {
@@ -128,6 +196,8 @@ fn GPIOTE() {
         }
     });
 
+    // grab a mutable reference to the Gpiote instance, determine which button sent the signal,
+    // reset the interrupt, and update the RESOULTION atomic if debounced timer as timed out
     GPIOTE_PERIPHERAL.with_lock(|gpiote| {
         if gpiote.channel0().is_event_triggered() {
             //A button press
@@ -146,12 +216,25 @@ fn GPIOTE() {
     });
 }
 
+/// Entry point
+///
+/// Set up the peripherals to be used and initialize the GPIO Events to trigger on either button press
+/// and pass into global Mutex handlers. TIMER 0 is dedicated to the display and TIMER 1 is used to protect
+/// aginst button bounce.
+///
+/// Becuase the LSM303AGR is attached via I2C to the MCU, the TWIM0 peripheral is used to communicate with the
+/// accelerometer within the LSM303AGR.
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
+
     let board = Board::take().unwrap();
+
+    // TIMER0 will be dedicated to the LED display
     let mut display_timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
+
+    // ensure buttons are in Floating mode
     let a_btn = board.buttons.button_a.into_floating_input();
     let b_btn = board.buttons.button_b.into_floating_input();
 
@@ -178,6 +261,7 @@ fn main() -> ! {
     debounce_timer.reset_event();
     DEBOUNCE_TIMER.init(debounce_timer);
 
+    // initialize the I2C TWIN0 communication with the accelerometer registers w/in the LSM303AGR
     let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
     let mut sensor = Lsm303agr::new_with_i2c(i2c);
     sensor.init().unwrap();
@@ -189,19 +273,23 @@ fn main() -> ! {
         )
         .unwrap();
 
+    // new LED state structure
     let mut leds = LEDs::new();
 
     // Set up the NVIC to handle interrupts.
-    unsafe { NVIC::unmask(Interrupt::GPIOTE) };
-    NVIC::unpend(Interrupt::GPIOTE);
+    unsafe { NVIC::unmask(Interrupt::GPIOTE) }; // allow NVIC to handle GPIOTE signals
+    NVIC::unpend(Interrupt::GPIOTE); //clear any currently pending GPIOTE state
 
     loop {
+        // read state from atomic RESOLUTION, casting to a BubbleResolution enum variant
         leds.set_mode(BubbleResolution::try_from(RESOLUTION.load(Acquire)).unwrap());
 
+        // in accelerometer data is new, grab the mG normalized X,Y,Z data and pass it
+        // into the LED::update method before rendering
         if sensor.accel_status().unwrap().xyz_new_data() {
             let (x, y, z) = sensor.acceleration().unwrap().xyz_mg();
-            //rprintln!("Acceleration: x {} y {} z {}", x, y, z);
 
+            // send state to LEDs
             display.show(
                 &mut display_timer,
                 leds.update(x, y, z),
